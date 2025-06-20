15cd1276f35d75377f1b751c469086322312b360
horoiwa <horoiwa195@gmail.com>
2025-06-21 02:43:12 +0900

    dev

--- src/tools/envs.py
+++ src/tools/envs.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import ale_py
 import gymnasium as gym
-from gymnasium.wrappers import AtariPreprocessing, TimeLimit, RecordVideo
+from gymnasium.wrappers import AtariPreprocessing, TimeLimit, FrameStack, RecordVideo
 
 
 ENV_INFO = {
@@ -14,35 +14,43 @@ ENV_INFO = {
 
 def get_atari_env(
     env_id: str,
-    video_folder: Optional[Path] = None,
     render_mode: str = "rgb_array",
+    video_folder: Optional[Path] = None,
 ) -> gym.Env:
     """Create and return an Atari environment with preprocessing."""
     env_name = ENV_INFO[env_id]["env_name"]
-    _env = gym.make(
-        env_name,
-        render_mode=render_mode,
-        frameskip=1,
-        repeat_action_probability=0.0,
-    )
-    if video_folder is not None:
-        _env = RecordVideo(
-            env=_env,
-            video_folder=video_folder,
-            episode_trigger=lambda x: True,
-        )
-
     env = TimeLimit(
-        AtariPreprocessing(
-            env=_env,
-            noop_max=10,
-            frame_skip=4,
-            terminal_on_life_loss=True,
-            screen_size=84,
-            grayscale_obs=True,
-            grayscale_newaxis=False,
-            scale_obs=True,
+        FrameStack(
+            AtariPreprocessing(
+                env=gym.make(
+                    env_name,
+                    render_mode=render_mode,
+                    frameskip=1,
+                    repeat_action_probability=0.0,
+                ),
+                noop_max=10,
+                frame_skip=4,
+                terminal_on_life_loss=True,
+                screen_size=84,
+                grayscale_obs=True,
+                grayscale_newaxis=False,
+                scale_obs=True,
+            ),
+            num_stack=4,
         ),
         max_episode_steps=2000,
     )
+
+    if video_folder is not None:
+        env = RecordVideo(
+            env=env,
+            video_folder=video_folder,
+            episode_trigger=lambda x: x % 25 == 0,
+        )
+
     return env
+
+
+def get_action_space(env_id: str) -> int:
+    """Return the action space size for the given environment."""
+    return gym.make(env_id).action_space.n
