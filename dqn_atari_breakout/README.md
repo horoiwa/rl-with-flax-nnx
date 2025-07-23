e457086a4b0fe91285239d638a02f5d798c336d1
horoiwa <horoiwa195@gmail.com>
2025-07-23 06:23:54 +0000

    setup

---
+++ dqn_atari_breakout/README.md
@@ -0,0 +1,18 @@
+
+
+```
+uv run python train.py
+```
+
+
+## References
+
+
+[Flax NNX](https://flax.readthedocs.io/en/v0.8.3/experimental/nnx/index.html)
+
+[DQN](https://web.stanford.edu/class/psych209/Readings/MnihEtAlHassibis15NatureControlDeepRL.pdf)
+
+[Gymnasium](https://gymnasium.farama.org/)
+
+[ALE](https://ale.farama.org/)
+
---
+++ dqn_atari_breakout/train.py
@@ -0,0 +1,26 @@
+import click
+import wandb
+from src import dqn
+
+@click.group()
+def cli():
+    pass
+
+@cli.command(name="dqn")
+@click.option('--use-wandb', is_flag=True, help='Enable wandb (default: disable)')
+def run_dqn(use_wandb: bool):
+    try:
+        wandb.init(
+            project="dqn",
+            mode="online" if use_wandb else "disabled",
+        )
+        dqn.main(env_id="Breakout-v4", outdir="log")
+    finally:
+        wandb.finish()
+
+
+if __name__ == "__main__":
+    cli()
+
+
+
