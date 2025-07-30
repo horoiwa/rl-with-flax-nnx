650561e3c85c802b3eaa91fb96470f00cb362386
horoiwa <horoiwa195@gmail.com>
2025-07-30 14:30:43 +0900

    dev

--- ppo_mujoco_unitreego1/src/ppo.py
+++ ppo_mujoco_unitreego1/src/ppo.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import shutil
+import functools
 
 import wandb
 
@@ -20,10 +21,15 @@ import orbax.checkpoint as ocp
 from mujoco_playground import wrapper, registry
 from mujoco_playground.config import locomotion_params
 
+UNROLL_LENGTH = 10
+GAMMA = 0.99
+GAE_LAMBDA = 0.95
+
 
 def create_env(env_id: str, num_envs: int = 1, auto_reset: bool = False):
     env, env_cfg = registry.load(env_id), registry.get_default_config(env_id)
     obs_dim: int = env.observation_size["state"][0]
+    priv_obs_dim: int = env.observation_size["privileged_state"][0]
     action_dim: int = env.action_size
     if auto_reset:
         env = wrapper.BraxAutoResetWrapper(env)
@@ -35,7 +41,7 @@ def create_env(env_id: str, num_envs: int = 1, auto_reset: bool = False):
         reset_fn = jax.jit(jax.vmap(env.reset, in_axes=(0,)))
         step_fn = jax.jit(jax.vmap(env.step, in_axes=(0, 0)))
 
-    return env, env_cfg, obs_dim, action_dim, reset_fn, step_fn
+    return env, env_cfg, obs_dim, priv_obs_dim, action_dim, reset_fn, step_fn
 
 
 class GaussianPolicy(nnx.Module):
@@ -68,11 +74,12 @@ class GaussianPolicy(nnx.Module):
 class ValueNN(nnx.Module):
     def __init__(self, obs_dim, rngs: nnx.Rngs):
 
-        self.dense_1 = nnx.Linear(in_features=obs_dim, out_features=128, rngs=rngs)
+        self.dense_1 = nnx.Linear(in_features=obs_dim, out_features=512, rngs=rngs)
         self.dense_2 = nnx.Linear(in_features=512, out_features=256, rngs=rngs)
         self.dense_3 = nnx.Linear(in_features=256, out_features=128, rngs=rngs)
         self.out = nnx.Linear(in_features=128, out_features=1, rngs=rngs)
 
+    @nnx.jit
     def __call__(self, x):
         x = nnx.elu(self.dense_1(x))
         x = nnx.elu(self.dense_2(x))
@@ -109,10 +116,16 @@ def train_step(
     return ploss.mean(), vloss.mean()
 
 
-# @nnx.jit
-def compute_advantage(rewards, dones, value_network, gamma=0.99):
+def compute_advantage_and_target(value_nn, obs, rewards, dones):
     """Computes advantage(GAE)"""
-    pass
+    B, T, _ = obs.shape
+    values = value_nn(obs.reshape(B * T, -1)).reshape(B, T)
+    values_t = values[:, :-1]
+    values_t_plus_1 = values[:, 1:]
+
+    deltas = (rewards + GAMMA * (1 - dones) * values_t_plus_1) - values_t
+
+    return
 
 
 def train(env_id: str, num_envs: int, log_dir: str):
@@ -121,39 +134,52 @@ def train(env_id: str, num_envs: int, log_dir: str):
         shutil.rmtree(log_dir)
     log_dir.mkdir(parents=True, exist_ok=True)
 
-    (env, env_cfg, obs_dim, action_dim, reset_fn, step_fn) = create_env(
-        env_id, num_envs=num_envs, auto_reset=True
+    (env, env_cfg, obs_dim, priv_obs_dim, action_dim, env_reset_fn, env_step_fn) = (
+        create_env(env_id, num_envs=num_envs, auto_reset=True)
     )
     ppo_params = locomotion_params.brax_ppo_config(env_id)
 
     policy_nn = GaussianPolicy(obs_dim=obs_dim, action_dim=action_dim, rngs=nnx.Rngs(0))
     policy_optimizer = optax.adam(learning_rate=1e-3)
 
-    value_nn = ValueNN(obs_dim=obs_dim, rngs=nnx.Rngs(0))
+    value_nn = ValueNN(obs_dim=priv_obs_dim, rngs=nnx.Rngs(0))
     value_optimizer = optax.adam(learning_rate=1e-3)
 
     rng, *keys = jax.random.split(jax.random.PRNGKey(0), num_envs + 1)
-    states = reset_fn(jnp.array(keys))
+    state = env_reset_fn(jnp.array(keys))
 
-    trajcetory = [states]
-    for i in range(100_000_000 // num_envs):
+    trajectory, actions = [state], []
+    for i in range(1, 100_000_000 // num_envs):
         rng, key = jax.random.split(rng)
-        actions = policy_nn.sample_action(states.obs["state"], key)
-        states = step_fn(states, actions)
-        trajcetory.append(states)
-        i += num_envs
+        action = policy_nn.sample_action(state.obs["state"], key)
+        state = env_step_fn(state, action)
 
-        if i % 4 == 0:
-            _trajectory, trajectory = trajectory[:5], trajcetory[4:]
-            batch_data = {
-                "obs": jnp.stack([s.obs["state"] for s in _trajectory]),
-                "actions": jnp.stack([s.action for s in _trajectory]),
-                "rewards": jnp.stack([s.reward for s in _trajectory]),
-                "dones": jnp.stack([s.done for s in _trajectory]),
-            }
-            batch_data["advantages"] = compute_advantage(
-                batch_data["obs"], batch_data["rewards"], batch_data["dones"], value_nn
+        actions.append(action)
+        trajectory.append(state)
+
+        if i % UNROLL_LENGTH == 0:
+            _traj, trajectory = (
+                trajectory[: UNROLL_LENGTH + 1],
+                trajectory[UNROLL_LENGTH:],
             )
+            batch_data = {
+                "obs_policy": jnp.stack([s.obs["state"] for s in _traj[:-1]], axis=1),
+                "obs_value": jnp.stack(
+                    [s.obs["privileged_state"] for s in _traj], axis=1
+                ),
+                "actions": jnp.stack(actions, axis=1),
+                "rewards": jnp.stack([s.reward for s in _traj[1:]], axis=1),
+                "dones": jnp.stack([s.done for s in _traj[1:]], axis=1),
+            }
+
+            advantages, targets = compute_advantage_and_target(
+                value_nn,
+                obs=batch_data["obs_value"],
+                rewards=batch_data["rewards"],
+                dones=batch_data["dones"],
+            )
+            batch_data["advantages"] = advantages
+            batch_data["value_targets"] = targets
 
             train_step(
                 batch_data,
@@ -164,7 +190,7 @@ def train(env_id: str, num_envs: int, log_dir: str):
             )
 
         if i % 1000 == 0:
-            score = evaluate(
+            test_score = evaluate(
                 env_id=env_id, n_episodes=5, log_dir=log_dir, record_video=False
             )
 
@@ -204,7 +230,7 @@ def evaluate(env_id: str, n_episodes: int, log_dir: str, record_video: bool = Tr
 if __name__ == "__main__":
     try:
         # wandb.init(project="ppo", mode="disabled")
-        train(env_id="Go1JoystickFlatTerrain", num_envs=4, outdir="log")
-        # evaluate(env_id="Go1JoystickFlatTerrain", outdir="log", n_episodes=5)
+        train(env_id="Go1JoystickFlatTerrain", num_envs=4, log_dir="log")
+        # evaluate(env_id="Go1JoystickFlatTerrain", log_dir="log", n_episodes=5)
     finally:
         wandb.finish()
